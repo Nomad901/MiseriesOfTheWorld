34730d2447369c9f027d6364c96e24c56abf9e67
Tango <maxim.ant07@gmail.com>
2025-08-25 20:24:35 +0300

    Update Renderer.cpp

--- src/Game/Renderer.cpp
+++ src/Game/Renderer.cpp
@@ -1,1 +1,13 @@
 #include "Renderer.h"
+
+Renderer::Renderer(SDL_Renderer* pRenderer)
+	: mRenderer(pRenderer)
+{}
+
+void Renderer::drawAll(uint32_t pDelay)
+{
+	mTimer.startTimer();
+
+
+	mTimer.setProperFPS(pDelay);
+}
