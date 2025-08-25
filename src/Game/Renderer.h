b53fd2c231492fe4ea4427c710e0acebedb45955
Tango <maxim.ant07@gmail.com>
2025-08-25 20:24:23 +0300

    Update Renderer.h

--- src/Game/Renderer.h
+++ src/Game/Renderer.h
@@ -1,5 +1,30 @@
 #pragma once
+#include <SDL/SDL.h>
+
+#include "Assets.h"
+#include "AnimatedTexture.h"
+#include "Game objets/FactoryObjects.h"
+#include "Game objets/FactoryOfEntities.h"
+#include "Game objets/FactoryOfFonts.h"
+#include "Time/Timer.h"
+
+//
+// temporary class, which is not used yet. will be set in the future
+//
+
 class Renderer
 {
+public:
+	Renderer(SDL_Renderer* pRenderer);
+
+	//
+	// invokes setImplementation and operates over FPS;
+	//
+	void drawAll(uint32_t pDelay);
+
+private:
+	SDL_Renderer* mRenderer{};
+	
+	Timer mTimer;
 };
 
